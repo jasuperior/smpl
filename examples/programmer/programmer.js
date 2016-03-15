98312222382f7be019379ff1b24515df9ec80b59
Ja <jamel.cole@gmail.com>
2016-03-15 11:59:33 -0400

    fixed grouping problem with multiple patterns. added programmer example

---
+++ examples/programmer/programmer.js
@@ -0,0 +1,30 @@
+function programmer(name, age, skills) {
+	this.name = name;
+	this.age = age;
+	this.skills = skills;
+};
+programmers are: [
+	"joe",
+	10,
+	"javascript"
+],
+	[
+	"james",
+	20,
+	"javascript,java"
+],
+	[
+	"jamie",
+	25,
+	"haskell,prolog"
+],
+	[
+	"jonathan",
+	16,
+	"c,c#,basic"
+],
+	[
+	"django",
+	21
+] for every programmers,
+	log name if skillset exists;
