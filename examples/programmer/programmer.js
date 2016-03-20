0fc3fd6c33a364edbee2033252c3c98b520c76e2
Ja <jamel.cole@gmail.com>
2016-03-20 10:34:20 -0400

    Created Branch to prepare for first stable release v0.1.0

--- examples/programmer/programmer.js
+++ examples/programmer/programmer.js
@@ -2,10 +2,35 @@ function Programmer(name, age, skills) {
 	this.name = name;
 	this.age = age;
 	this.skills = skills;
-};
-Programmer(jamel);
-Programmer(tony);
-Programmer(steven);
+}
 
-for every Programmer in programmers,
-	log name if skillset exists;
+var createProgrammer = (function () {
+		function create(args) {
+			return Programmer.apply(this, args)
+		}
+
+		create.prototype = Programmer.prototype;
+
+		return function () {
+			return new create(arguments)
+		}
+	})();
+
+var programers = [
+		createProgrammer.apply(null, [
+			"jamel",
+			10,
+			"nothing"
+		]),
+		createProgrammer.apply(null, [
+			"james",
+			20,
+			"noway"
+		])
+	];
+
+programmers.forEach((Programmer) = > {
+	console
+});.if(skillset, exists) {
+	return log(name)
+};
